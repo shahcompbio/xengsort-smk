73661368d23a0d5ac242566d72fbb1ae9e1f025b
apsteinberg <asherps@gmail.com>
2024-10-16 16:24:00 -0400

    MSK commits

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # xengsort-smk
-Snakemake pipeline for xengsort
+Snakemake pipeline for xengsort -- disambiguate mouse from human reads directly on fastq files:
+
+https://gitlab.com/genomeinformatics/xengsort
---
+++ config/run_full_workflow.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --partition=componc_cpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=48:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=xengsort
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=preskaa@mskcc.org
+#SBATCH --output=slurm%j_xengsort_snkmk.out
+
+### example slurm submission script ###
+
+## load modules
+module load singularity/3.7.1
+## set directories
+tag=APS017.1
+pipeline_dir=$HOME/fragpipe-smk
+#logdir=${submitdir}/log
+outdir=/data1/shahs3/users/preskaa/${tag}_Archive/
+config_yaml=config/config.yml
+profile_yaml=${pipeline_dir}/workflow/profiles/
+snakefile=${pipeline_dir}/workflow/Snakefile
+## switch to the right conda environment
+source /home/preskaa/miniconda3/bin/activate snakemake
+
+mkdir -p ${outdir}
+
+### run the pipeline
+echo "Current working directory: $(pwd)"
+echo "Snakefile path: ${snakefile}"
+
+# this will run the process, for example you could switch the below to a python command:
+# python script.py
+cd ${pipeline_dir}
+
+## if using conda, include flag:
+## --conda-prefix /data1/shahs3/users/preskaa/conda
+snakemake \
+  --snakefile ${snakefile} \
+  --profile ${profile_yaml} \
+  --configfile ${config_yaml}\
+  --conda-prefix /data1/shahs3/users/preskaa/conda \
+  --singularity-prefix /data1/shahs3/users/preskaa/singularity \
+  --singularity-args "--bind /data1/shahs3" \
+  --dry-run
+
+
+
+
+
